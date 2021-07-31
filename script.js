6735ed31d94f7b4c4b8732683af54f8367eef6ad
S Macleod <toma98so@gmail.com>
2021-07-31 16:44:44 +0300

    feat(script.js): for lesson 2

--- script.js
+++ script.js
@@ -2,8 +2,25 @@
 let money,
 income,
 addExpenses, 
-deposit,
 mission,
 period;
-alert("Hello");
-console.log("Test");
+
+
+money = 1000;
+income = "main work";
+addExpenses = "food, internet";
+mission = 10000;
+period = 1;
+let deposit = true;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log("Период равен", period, "месяцев"); 
+console.log("Цель заработать", mission, "рублей");
+console.log(addExpenses.toLocaleLowerCase());
+console.log(addExpenses.split(', '));
+let budgetDay;
+budgetDay = 900;
+console.log("budgetDay", budgetDay, "рублей"); 
