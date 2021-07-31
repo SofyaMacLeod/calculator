115379fb99b08fd7796c030558329371b56d205a
S Macleod <toma98so@gmail.com>
2021-07-31 18:38:46 +0300

    fix(script.js):mistakes

--- script.js
+++ script.js
@@ -1,26 +1,16 @@
 /*jshint esversion: 6 */
-let money,
-income,
-addExpenses, 
-mission,
-period;
+const 
+  money = 1000,
+  income = "main work",
+  addExpenses = "food, internet",
+  mission = 10000,
+  period = 1,
+  deposit = true,
+  budgetDay = 900;
 
-
-money = 1000;
-income = "main work";
-addExpenses = "food, internet";
-mission = 10000;
-period = 1;
-let deposit = true;
-
-console.log(typeof money);
-console.log(typeof income);
-console.log(typeof deposit);
+console.log(typeof money, typeof income, typeof deposit); 
 console.log(addExpenses.length);
 console.log("Период равен", period, "месяцев"); 
 console.log("Цель заработать", mission, "рублей");
-console.log(addExpenses.toLocaleLowerCase());
-console.log(addExpenses.split(', '));
-let budgetDay;
-budgetDay = 900;
+console.log(addExpenses.toLocaleLowerCase(), addExpenses.split(', '));
 console.log("budgetDay", budgetDay, "рублей"); 
