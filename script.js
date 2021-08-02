ce308bff81457d58836e11dc7c451cc4ac3f347b
S Macleod <toma98so@gmail.com>
2021-08-02 22:52:50 +0300

    fix(script.js):mistakes

--- script.js
+++ script.js
@@ -5,25 +5,22 @@ const
   mission = 100000,
   period = 1;
 
-  let money = prompt('Ваш месячный доход');
-  let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
-  let deposit = confirm('Есть ли у вас депозит в банке?');
-
+  const money = prompt('Ваш месячный доход');
+  const addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+  const deposit = confirm('Есть ли у вас депозит в банке?');
+  const expenses1 = prompt('Введите обязательную статью расходов?');
+  const amount1 = prompt('Во сколько это обойдется?');
+  const expenses2 = prompt('Введите обязательную статью расходов?');
+  const amount2 = prompt('Во сколько это обойдется?');
+  const budgetMonth = money - amount1 - amount2;
+  const budgetDay = (money - amount1 - amount2)/30;
+  
   console.log(typeof money, typeof income, typeof deposit); 
   console.log(addExpenses.length);
   console.log("Период равен", period, "месяцев"); 
   console.log("Цель заработать", mission, "рублей");
   console.log(addExpenses.toLocaleLowerCase().split(', '));
-  
-
-  let expenses1 = prompt('Введите обязательную статью расходов?');
-  let amount1 = prompt('Во сколько это обойдется?');
-  let expenses2 = prompt('Введите обязательную статью расходов?');
-  let amount2 = prompt('Во сколько это обойдется?');
-
-  let budgetMonth = money - amount1 - amount2;
   console.log("Бюджет месяц", budgetMonth, "рублей"); 
-  let budgetDay = (money - amount1 - amount2)/30;
   console.log("Бюджет на день", budgetDay, "рублей"); 
   if (budgetDay > 1200) {console.log("У вас высокий уровень дохода");}
     else if (budgetDay > 600, budgetDay <= 1200) {console.log("У вас средний уровень дохода");}
