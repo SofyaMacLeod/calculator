575b1522dc41f0573c18b601bdb2f7c807e69c0a
S Macleod <toma98so@gmail.com>
2021-08-02 16:09:07 +0300

    feat(script.js):start

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 /*jshint esversion: 6 */
+'use sctrict';
 const 
   money = 1000,
   income = "main work",
@@ -8,9 +9,17 @@ const
   deposit = true,
   budgetDay = 900;
 
-console.log(typeof money, typeof income, typeof deposit); 
-console.log(addExpenses.length);
-console.log("Период равен", period, "месяцев"); 
-console.log("Цель заработать", mission, "рублей");
-console.log(addExpenses.toLocaleLowerCase().split(', '));
-console.log("budgetDay", budgetDay, "рублей"); 
+  console.log(typeof money, typeof income, typeof deposit); 
+  console.log(addExpenses.length);
+  console.log("Период равен", period, "месяцев"); 
+  console.log("Цель заработать", mission, "рублей");
+  console.log(addExpenses.toLocaleLowerCase().split(', '));
+  console.log("budgetDay", budgetDay, "рублей"); 
+
+  let question = prompt('Ваш месячный доход');
+  let question2 = prompt('Перечислите возможные расходы за рассчитываемый период через запятую');
+  let question3 = prompt('Есть ли у вас депозит в банке?');
+  let question4 = prompt('Введите обязательную статью расходов?');
+  let question5 = prompt('Во сколько это обойдется?');
+  let question6 = prompt('Введите обязательную статью расходов?');
+  let question7 = prompt('Во сколько это обойдется?');
